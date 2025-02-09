a7dbb7d0414638c0aa05909dc8f9e3004ab8ed7e
Benjamin Powell <benjaminpowell24@gmail.com>
2025-02-09 18:59:48 +0000

    remove unused helper

--- smart-assistant-service/src/server.js
+++ smart-assistant-service/src/server.js
@@ -1,16 +1,12 @@
 import './tracer.js'
 import 'dotenv/config'
 import express from 'express'
-import minimist from 'minimist'
 import conversation from './routes/conversation.js'
 import cors from 'cors'
 import { CONFIG } from './utils/config.js'
 
-const argv = minimist(process.argv.slice(2))
-
 import { logger } from './logger.js'
 
-const host = argv.host || '127.0.0.1'
 const port = CONFIG.PORT || 3000
 
 const server = express()
@@ -25,10 +21,7 @@ server.use(`${baseURI}/conversation`, conversation)
   try {
       server.listen(port, () => {
         logger.info(
-            'Server listening on ' +
-            host +
-            ':' +
-            port
+            `Server listening on localhost: ${port}`
         )
       })
   } catch (e) {
