f8a78710fbebbf3f0d8fb0ab9e5035c4b1580bcc
Benjamin Powell <benjaminpowell24@gmail.com>
2025-05-31 19:15:48 +0000

    chore: fix base path

--- smart-assistant/vite.config.ts
+++ smart-assistant/vite.config.ts
@@ -2,23 +2,21 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-    const env = loadEnv(mode, process.cwd())
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
 
   return {
-     base: '/smart-assistant/',
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/v1/smart-assistant': {
-        target: env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
+    base: '/',
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/v1/smart-assistant': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
   }
- 
 })
-
